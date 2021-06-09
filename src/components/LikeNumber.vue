1ba50be011f3cbb1c9e28366e4423c02e45ef851
shushikata <shu.42111227@gmail.com>
2021-06-09 18:58:39 +0900

    Vue.js サンプルアプリケーションを作成

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>いいね ({{ totalNumber / 2 }})</p>
+    <button @click="increment">UP</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      totalNumber: {
+        type: Number,
+        required: true,
+      },
+    },
+    methods: {
+      increment() {
+        this.$emit("my-click", this.totalNumber + 1);
+      }
+    }
+  }
+</script>
